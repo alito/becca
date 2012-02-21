65caeb8eaa7bea39a61c8dac3b9d4a2bd151a9ab
Alejandro Dubrovsky <alito@organicrobot.com>
2012-02-22 03:57:05 +1100

    tester: bit of tidying up

--- tester.py
+++ tester.py
@@ -18,22 +18,53 @@ Default_Save_Period = 20000
 
 Default_World_Class = 'worlds.grid_1D.Grid_1D'
 
-#from worlds.grid_1D import Grid_1D as worldcreator
-#from worlds.grid_1D_ms import Grid_1D_ms as worldcreator
-#from worlds.grid_1D_noise import Grid_1D_noise as worldcreator
-#from worlds.grid_2D import Grid_2D as worldcreator
-#from worlds.grid_2D_dc import Grid_2D_dc as worldcreator
-#from worlds.image_1D import Image_1D as worldcreator
-#
-#    import worlds.listen
-
-#    import worlds.morris  # under development
-
-#from agent.agent_stub import Agent
 from agent.agent import Agent
 from agent.agent_stub import Agent as RandomAgent
 
 from experiment.experiment import Experiment
+                    
+
+def main(args):
+    parser = argparse.ArgumentParser(description='tester')
+    parser.add_argument('world_creator', nargs="?", default=Default_World_Class,
+                        help="Class to create world (default: %(default)s)")
+    parser.add_argument('--dummy-agent', dest="dummy_agent", default=False, action="store_true",
+                        help="Use an agent that just returns random actions. Good for testing worlds")    
+    parser.add_argument('-s', '--save-period', dest="save_period", type=int, default=Default_Save_Period,
+                        help="How often to take snapshots (default: %(default)s)")
+    parser.add_argument('-p', '--pickle-prefix', dest="pickle_prefix", default=None,
+                        help="Use pickle prefix specified")
+    parser.add_argument('-v', '--verbosity', dest="verbosity", default=1, type=int,
+                        help="How verbose to be (default: %(default)s)")
+
+    parameters = parser.parse_args(args)
+
+    if parameters.verbosity <= 0:
+        logging_level = logging.ERROR
+    elif parameters.verbosity == 1:
+        logging_level = logging.WARNING
+    elif parameters.verbosity == 2:
+        logging_level = logging.INFO
+    elif parameters.verbosity == 3:
+        logging_level = logging.DEBUG
+
+    logging.basicConfig(level=logging_level, format="%(asctime)s %(levelname)-8s %(message)s")
+
+    if parameters.dummy_agent:
+        agent_class = RandomAgent
+    else:
+        agent_class = Agent
+
+    world_creator = get_world_class(parameters.world_creator)
+    if world_creator is None:
+        parser.error("Couldn't find world creator %s" % parameters.world_creator)
+        
+    experiment = Experiment(world_creator, agent_class, pickle_prefix=parameters.pickle_prefix,
+                            backup_period=parameters.save_period)
+    experiment.run()
+
+    return 0
+
 
 def get_world_class(world_name):
     """
@@ -128,48 +159,7 @@ def get_world_class(world_name):
     return klass
 
                         
-                    
 
-def main(args):
-    parser = argparse.ArgumentParser(description='tester')
-    parser.add_argument('world_creator', nargs="?", default=Default_World_Class,
-                        help="Class to create world (default: %(default)s)")
-    parser.add_argument('--dummy-agent', dest="dummy_agent", default=False, action="store_true",
-                        help="Use an agent that just returns random actions. Good for testing worlds")    
-    parser.add_argument('-s', '--save-period', dest="save_period", type=int, default=Default_Save_Period,
-                        help="How often to take snapshots (default: %(default)s)")
-    parser.add_argument('-p', '--pickle-prefix', dest="pickle_prefix", default=None,
-                        help="Use pickle prefix specified")
-    parser.add_argument('-v', '--verbosity', dest="verbosity", default=1, type=int,
-                        help="How verbose to be (default: %(default)s)")
 
-    parameters = parser.parse_args(args)
-
-    if parameters.verbosity <= 0:
-        logging_level = logging.ERROR
-    elif parameters.verbosity == 1:
-        logging_level = logging.WARNING
-    elif parameters.verbosity == 2:
-        logging_level = logging.INFO
-    elif parameters.verbosity == 3:
-        logging_level = logging.DEBUG
-
-    logging.basicConfig(level=logging_level, format="%(asctime)s %(levelname)-8s %(message)s")
-
-    if parameters.dummy_agent:
-        agent_class = RandomAgent
-    else:
-        agent_class = Agent
-
-    world_creator = get_world_class(parameters.world_creator)
-    if world_creator is None:
-        parser.error("Couldn't find world creator %s" % parameters.world_creator)
-        
-    experiment = Experiment(world_creator, agent_class, pickle_prefix=parameters.pickle_prefix,
-                            backup_period=parameters.save_period)
-    experiment.run()
-
-    return 0
-    
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
