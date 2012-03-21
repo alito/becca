301f155f05d99c6090ef8859968dafe6929e34b2
Brandon Rohrer <brohrer@gmail.com>
2012-03-20 22:17:55 -0600

    Big changes throughout becca, including both worlds and agent

--- tester.py
+++ tester.py
@@ -2,14 +2,18 @@
 A main test harness for a general reinforcement learning agent. 
 """
 
-import numpy
+import numpy as np
 
+#from agent.agent_stub import AgentStub as Agent
+from agent.agent import Agent
+        
 """  Selects the World that the Agent will be placed in. One of these
 lines should be uncommented.
 """
-from . import worlds.grid_1D.Grid_1D as World
-#import worlds.grid_1D.Grid_1D_ms as world
-#import worlds.grid_1D.Grid_1D_noise as world
+#from worlds.base_world import World
+from worlds.grid_1D import World
+#from worlds.grid_1D import Grid_1D_ms as World
+#from worlds.grid_1D import Grid_1D_noise as World
 
 def main():
     
@@ -18,13 +22,11 @@ def main():
     """ A unique identifying string for the agent, allowing specific 
     saved agents to be recalled. 
     """
-    agent_name = "test_agent";
-    #from agent.agent import Agent
-    # debug against agent_stub
-    from agent.agent_stub import AgentStub as Agent
+    agent_name = "test";
     agent = Agent(agent_name, world.num_sensors, world.num_primitives, 
                   world.num_actions)
-
+    agent = agent.restore()
+    
     """ If configured to do so, the world sets some Becca parameters to 
     modify its behavior. This is a development hack, and should eventually be 
     removed as Becca matures and settles on a good, general purpose
@@ -33,15 +35,16 @@ def main():
     world.set_agent_parameters(agent)
          
     """ Give an initial resting action to kick things off. """
-    actions = numpy.zeros(world.num_actions)
-
+    actions = np.zeros(world.num_actions)
+    
     """ Repeat the loop through the duration of the existence of the world."""
-    while(world.timestep < world.LIFESPAN):
+    while(world.is_alive()):
         sensors, primitives, reward = world.step(actions)
         actions = agent.step(sensors, primitives, reward)
              
     """ Report the performance of the agent on the world. """
-    print('final performance is {0:.3f}'.format(agent.final_performance()))
+    agent.report_performance()
+    agent.show_reward_history(show=False)
     
     return 0
     
